58dd335d80f738cbb16c8ccdf3087fa9ae04402c
Kevin Mei <kevjinx@gmail.com>
2021-04-19 16:49:59 -0700

    fix html

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
-const box1 = document.getElementById('box1')
-const box2 = document.getElementById('box2')
-const box3 = document.getElementById('box3')
-const box4 = document.getElementById('box4')
-const box5 = document.getElementById('box5')
-const box6 = document.getElementById('box6')
-const box7 = document.getElementById('box7')
-const box8 = document.getElementById('box8')
-const box9 = document.getElementById('box9')
-
-box1.addEventListener('click', changeNum);
-box2.addEventListener('click', changeNum);
-box3.addEventListener('click', changeNum);
-box4.addEventListener('click', changeNum);
-box5.addEventListener('click', changeNum);
-box6.addEventListener('click', changeNum);
-box7.addEventListener('click', changeNum);
-box8.addEventListener('click', changeNum);
-box9.addEventListener('click', changeNum);
+const box1 = document.getElementById('box1').value;
+const box2 = document.getElementById('box2').value;
+const box3 = document.getElementById('box3').value;
+const box4 = document.getElementById('box4').value;
+const box5 = document.getElementById('box5').value;
+const box6 = document.getElementById('box6').value;
+const box7 = document.getElementById('box7').value;
+const box8 = document.getElementById('box8').value;
+const box9 = document.getElementById('box9').value;
 
 const boxArr = [box1,box2,box3,box4,box5,box6,box7,box8,box9]
-
-
-const changeNum = () => {
-
-}
-
-
-
-const
