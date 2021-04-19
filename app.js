0cb9479abcd010a78bfd314c8fc03ef9e8160679
Kevin Mei <kevjinx@gmail.com>
2021-04-19 16:01:16 -0700

    update html elements

--- app.js
+++ app.js
@@ -8,4 +8,23 @@ const box7 = document.getElementById('box7')
 const box8 = document.getElementById('box8')
 const box9 = document.getElementById('box9')
 
+box1.addEventListener('click', changeNum);
+box2.addEventListener('click', changeNum);
+box3.addEventListener('click', changeNum);
+box4.addEventListener('click', changeNum);
+box5.addEventListener('click', changeNum);
+box6.addEventListener('click', changeNum);
+box7.addEventListener('click', changeNum);
+box8.addEventListener('click', changeNum);
+box9.addEventListener('click', changeNum);
+
 const boxArr = [box1,box2,box3,box4,box5,box6,box7,box8,box9]
+
+
+const changeNum = () => {
+
+}
+
+
+
+const
